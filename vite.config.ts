5d12a3f98b1686799b8b79bc569ead09fba103fc
Bourzgui <bourzguifatimazahra@gmail.com>
2026-09-02 10:51:16 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
@@ -11,7 +10,6 @@ export default defineConfig({
       server: { entry: "server" },
     }),
     react(),
-    netlify(),
   ],
   resolve: {
     tsconfigPaths: true,
